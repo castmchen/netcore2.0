7357124f4e065678c539a522da6ae1fc553ebf71
castmchen <chenqiang_590@163.com>
2018-11-02 10:02:05 +0800

    summit

---
+++ Chapter09/TicTacToe/Data/GameDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Data
+{
+    public class GameDbContextFactory : IDesignTimeDbContextFactory<GameDbContext>
+    {
+        public GameDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<GameDbContext>();
+            optionsBuilder.UseSqlServer(@"Server=Localhost\SQL2014;Database=TicTacToe;Trusted_Connection=True;MultipleActiveResultSets=true;Persist Security Info=True;Integrated Security=SSPI;");
+            return new GameDbContext(optionsBuilder.Options);
+        }
+    }
+}
